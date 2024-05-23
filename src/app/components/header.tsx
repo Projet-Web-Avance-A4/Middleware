6975831e711d5596be4ed174ff3be6ea2ade3a95
Maxence <maxence.arcelin@viacesi.fr>
2024-05-24 01:53:22 +0200

    Added cards to index

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -14,35 +14,19 @@ export default function Header() {
             <NavbarBrand>
                 <p className="font-bold text-inherit">CES&apos;Eat</p>
             </NavbarBrand>
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Commandes
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link href="#" aria-current="page">
-                        Livraisons
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Articles
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Menu
-                    </Link>
-                </NavbarItem>
+            <NavbarContent justify="center">
+                <p>Restaurateur</p>
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem className="hidden lg:flex">
                     <Link href="#">Statistiques</Link>
                 </NavbarItem>
                 <NavbarItem>
-                    <Button as={Link} color="secondary" href="#">
-                        Mon compte
+                    <Button as={Link} href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                        </svg>
+                        <p>Mon compte</p>
                     </Button>
                 </NavbarItem>
             </NavbarContent>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,65 @@ import Image from "next/image";
 import Header from "./components/header";
 import Link from "next/link";
 import { Button } from "@nextui-org/button";
-import { Card, CardBody } from "@nextui-org/card";
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import { Component } from "react";
 
 export default function Home() {
   return (
-    <Header/>
+    <div className="h-screen">
+      <Header />
+
+      <div className="bg-beige">
+        <div className="container mx-auto">
+          <div className="flex flex-wrap place-content-center">
+            <Card className="m-8">
+              <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <h4 className="font-bold text-large">Commandes</h4>
+                <small className="text-default-500">Suivre et valider des commandes</small>
+              </CardHeader>
+              <CardBody>
+                <Button as={Link} href="#">
+                  <p>Accéder</p>
+                </Button>
+              </CardBody>
+            </Card>
+
+            <Card className="m-8">
+              <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <h4 className="font-bold text-large">Articles</h4>
+                <small className="text-default-500">Créer, modifier, supprimer et consulter un article</small>
+              </CardHeader>
+              <CardBody>
+                <Button as={Link} href="#">
+                  <p>Accéder</p>
+                </Button>
+              </CardBody>
+            </Card>
+            <Card className="m-8">
+              <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <h4 className="font-bold text-large">Menus</h4>
+                <small className="text-default-500">Créer, modifier, supprimer et consulter un menu</small>
+              </CardHeader>
+              <CardBody>
+                <Button as={Link} href="#">
+                  <p>Accéder</p>
+                </Button>
+              </CardBody>
+            </Card>
+            <Card className="m-8">
+              <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                <h4 className="font-bold text-large">Livraisons</h4>
+                <small className="text-default-500">Suivre des livraisons</small>
+              </CardHeader>
+              <CardBody>
+                <Button as={Link} href="#">
+                  <p>Accéder</p>
+                </Button>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
