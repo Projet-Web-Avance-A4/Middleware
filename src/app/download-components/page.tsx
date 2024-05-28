9a3588ed2242907a466449fddb52f39e88186ba5
Arthur Joye <arthurjoye0701@gmail.com>
2024-05-28 11:51:47 +0200

    style(download) : add style to download component page

--- src/app/download-components/page.tsx
+++ src/app/download-components/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
+import Header from "../components/header";
+import { NextUIProvider } from "@nextui-org/system";
+import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { Button } from '@nextui-org/button';
+import Link from "next/link";
 
 const DownloadComponentsPage: React.FC = () => {
   const [files, setFiles] = useState<any[]>([]);
@@ -24,18 +29,28 @@ const DownloadComponentsPage: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Téléchargement des Composants</h1>
-      <ul>
-        {files.map(file => (
-          <li key={file.filename}>
-            <a href={`/api/components?filename=${encodeURIComponent(file.filename)}`} download>
-              {file.filename}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <NextUIProvider className="h-screen bg-beige">
+      <Header />
+      <div className="container mx-auto">
+        <h1 className="font-bold text-large text-black text-center">Téléchargement des Composants</h1>
+        <ul>
+          {files.map(file => (
+            <li className='place-self-center' key={file.filename}>
+              <Card className="m-8 w-80">
+                <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
+                  <h4 className="font-bold text-large self-center">{file.filename}</h4>
+                </CardHeader>
+                <CardBody>
+                  <Button as={Link} href={`/api/components?filename=${encodeURIComponent(file.filename)}`}>
+                    Télécharger
+                  </Button>
+                </CardBody>
+              </Card>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </NextUIProvider >
   );
 };
 
