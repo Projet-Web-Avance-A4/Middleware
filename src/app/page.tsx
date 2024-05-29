a99196257e9cf2bf055fa1e2bcbdd18b6be91c88
Vincent LELEU <vincent.leleu@viacesi.fr>
2024-05-29 15:51:12 +0200

    add component footer and credits pop-up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,20 @@
 
 import { NextUIProvider } from "@nextui-org/system";
 import Header from "./components/header";
+import Footer from "./components/footer";
 import CustomCard from "./components/customcard";
 
 export default function Home() {
   return (
-    <NextUIProvider className="h-screen bg-beige">
-      <Header />
-      <div className="container mx-auto">
+    <NextUIProvider className=" flex flex-col min-h-screen bg-beige">
+      <Header/>
+      <div className="flex-grow container mx-auto">
         <div className="flex flex-wrap place-content-center">
           <CustomCard title="Commandes" description="Suivre et valider des commandes" href="#" btnText="Accéder" />
-          <CustomCard title="Articles" description="Créer, modifier, supprimer et consulter un article" href="#" btnText="Accéder" />
-          <CustomCard title="Menus" description="Créer, modifier, supprimer et consulter un menu" href="#" btnText="Accéder" />
-          <CustomCard title="Livraisons" description="Suivre des livraisons" href="#" btnText="Accéder" />
+          
         </div>
       </div>
+      <Footer/>
     </NextUIProvider>
   );
 }
