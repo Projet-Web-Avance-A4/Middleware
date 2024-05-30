1daa3fdffd6fa344123555045c102610ffac4ef2
Vincent LELEU <vincent.leleu@viacesi.fr>
2024-05-30 10:55:56 +0200

    add component portalCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@ import { NextUIProvider } from "@nextui-org/system";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import CustomCard from "./components/customcard";
+import PortalCard from "./components/portalCard";
 
 export default function Home() {
   return (
     <NextUIProvider className=" flex flex-col min-h-screen bg-beige">
       <Header title="Restaurateur" showStats={true} showSponsor={true}/>
-      <div className="flex-grow container mx-auto">
-        <div className="flex flex-wrap place-content-center">
-          <CustomCard title="Commandes" description="Suivre et valider des commandes" href="#" btnText="Accéder" />
-          
+      <div className="flex-grow container ">
+        <div className="grid place-content-center">
+   {/*        <CustomCard title="Commandes" description="Suivre et valider des commandes" href="#" btnText="Accéder" /> */}
+          <PortalCard href="#" btnText="Client" />
+          <PortalCard href="#" btnText="Client" />
+          <PortalCard href="#" btnText="Client" />
+          <PortalCard href="#" btnText="Client" />
         </div>
       </div>
       <Footer/>
