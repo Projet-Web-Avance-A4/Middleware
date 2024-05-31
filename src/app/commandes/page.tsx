d1fb0574253288c74dc415b1814336e1c44c94ee
Maxence <maxence.arcelin@viacesi.fr>
2024-05-31 16:15:53 +0200

    Created table component with mockup data on Commandes page

--- src/app/commandes/page.tsx
+++ src/app/commandes/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from "@nextui-org/table";
+import CustomTable from "../components/table";
 import { Card, CardBody, CardHeader, NextUIProvider, getKeyValue } from "@nextui-org/react";
 import Header from "../components/header";
+import { FaBoxesStacked } from "react-icons/fa6";
+
 
 const mockupRows = [
     {
@@ -53,21 +55,10 @@ export default function Commandes() {
             <div className="container mx-auto">
                 <Card className="m-8">
                     <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                        <h4 className="font-bold text-large">Commandes</h4>
+                        <h4 className="flex items-center font-bold text-large gap-2"><FaBoxesStacked/>Commandes</h4>
                     </CardHeader>
                     <CardBody>
-                        <Table>
-                            <TableHeader columns={columns}>
-                                {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
-                            </TableHeader>
-                            <TableBody items={mockupRows}>
-                                {(item) => (
-                                    <TableRow key={item.key}>
-                                        {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
-                                    </TableRow>
-                                )}
-                            </TableBody>
-                        </Table>
+                        <CustomTable/>
                     </CardBody>
                 </Card>
             </div>
