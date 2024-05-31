9cf8a4903753e94919bab864ff22acf934c9d51b
Vincent LELEU <vincent.leleu@viacesi.fr>
2024-05-31 16:21:04 +0200

    update customcard component

--- src/app/components/customcard.tsx
+++ src/app/components/customcard.tsx
@@ -4,22 +4,28 @@ import { Button } from "@nextui-org/button";
 import { NextUIProvider } from "@nextui-org/react";
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
 import Link from "next/link";
+import { ReactNode } from "react";
+import React from "react";
 
 interface iCustomCard {
   title: string
   description?: string
   href: string
   btnText: string
+  icon?: ReactNode
 }
 
 // Composant Card
 export default function CustomCard(props: iCustomCard) {
+
     return (
         <NextUIProvider>
             <Card className="m-8">
               <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                <h4 className="font-bold text-large">{props.title}</h4>
+                <div>
+                <h4 className="font-bold text-large flex items-center gap-2">{props.icon}{props.title}</h4>
                 <p className="text-default-500">{props.description}</p>
+                </div>
               </CardHeader>
               <CardBody>
                 <Button as={Link} href={props.href}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,15 @@ import Header from "./components/header";
 import Footer from "./components/footer";
 import CustomCard from "./components/customcard";
 import PortalCard from "./components/portalCard";
+import { FaUserLarge , FaChartColumn } from 'react-icons/fa6';
 
 export default function Home() {
   return (
     <NextUIProvider className=" flex flex-col min-h-screen bg-beige">
       <Header title="Portail de redirection" showMyAccount={false} showStats={false} showSponsor={false}/>
-      <div className="flex-grow container ">
-        <div className="grid place-content-center">
-   {/*        <CustomCard title="Commandes" description="Suivre et valider des commandes" href="#" btnText="Accéder" /> */}
-          <PortalCard href="#" btnText="Client" />
-          <PortalCard href="#" btnText="Client" />
-          <PortalCard href="#" btnText="Client" />
-          <PortalCard href="#" btnText="Client" />
-        </div>
+      <div className="grid flex-grow place-content-center">
+          <CustomCard title="Clients" href="#clientpage" btnText="Accéder" icon={<FaUserLarge className="w-10 h-10" />}/>
+          <CustomCard title="Dashboard" href="#dashboardpage" btnText="Accéder" icon={<FaChartColumn className="w-10 h-10"/>}/>
       </div>
       <Footer/>
     </NextUIProvider>
