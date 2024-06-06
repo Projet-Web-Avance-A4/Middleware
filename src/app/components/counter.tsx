031d604bab898831b6d6d487e86e12db46ac1056
Vincent LELEU <vincent.leleu@viacesi.fr>
2024-06-06 14:38:35 +0200

    modify counter background color :p

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -6,7 +6,7 @@ type CounterProps = {
 
 const Counter: React.FC<CounterProps> = ({ totalOrderPrice }) => {
   return (
-    <div className="flex flex-col items-center justify-center p-4 bg-blue-500 rounded-lg shadow-md w-auto mx-2 my-2">
+    <div className="flex flex-col items-center justify-center p-4 bg-darkGreen rounded-lg shadow-md w-auto mx-2 my-2">
       <h1 className="text-2xl font-bold mb-4">Chiffre d'affaires transactionnel global en cours</h1>
       <div className="text-3xl font-bold mb-4">{totalOrderPrice} € </div>
     </div>
