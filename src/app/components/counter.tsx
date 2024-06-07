01a8edbc96b5a34b7525b7b08eb04f65f19e9867
Vincent LELEU <vincent.leleu@viacesi.fr>
2024-06-07 16:46:46 +0200

    change apostrophe in counter component

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -7,7 +7,7 @@ type CounterProps = {
 const Counter: React.FC<CounterProps> = ({ totalOrderPrice }) => {
   return (
     <div className="flex flex-col items-center justify-center p-4 bg-darkGreen rounded-lg shadow-md w-auto mx-2 my-2">
-      <h1 className="text-2xl font-bold mb-4">Chiffre d'affaires transactionnel global en cours</h1>
+      <h1 className="text-2xl font-bold mb-4">Chiffre d&apos;affaires transactionnel global en cours</h1>
       <div className="text-3xl font-bold mb-4">{totalOrderPrice} € </div>
     </div>
   );
