94ac4eb5c010d221bf90574e31c3e8709fca5d29
Vincent LELEU <vincent.leleu@viacesi.fr>
2024-06-17 11:31:29 +0200

    change ports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
       <Header title="Portail de redirection" showStats={false} showAccount={false} />
       <div className="flex-grow container">
         <div className="grid place-content-center">
-          <PortalCard href={baseURL} port="3002" btnText="Client" />
-          <PortalCard href={baseURL} port="3007" btnText="Restaurateur" />
+          <PortalCard href={baseURL} port="3001" btnText="Client" />
+          <PortalCard href={baseURL} port="3006" btnText="Restaurateur" />
           <PortalCard href={baseURL} port="3004" btnText="Livreur" />
           <PortalCard href={baseURL} port="3003" btnText="Développeur" />
-          <PortalCard href={baseURL} port="3008" btnText="Service technique" />
-          <PortalCard href={baseURL} port="3001" btnText="Service commercial" />
+          <PortalCard href={baseURL} port="3007" btnText="Service technique" />
+          <PortalCard href={baseURL} port="3002" btnText="Service commercial" />
         </div>
       </div>
       <Footer />
